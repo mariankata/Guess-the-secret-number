486cfa1b94c928fc7931dfd1539ac91e7a8fa80b
Marian Andreev <mariankata@gmail.com>
2017-10-24 21:31:46 +0200

    upload files

---
+++ Guess the secret number.py
@@ -0,0 +1,7 @@
+secret = 93
+guess = int(raw_input("Guess the secret number: "))
+
+if secret == "guess":
+    print "Congratulations!!! You guessed the secret number :)"
+else :
+    print "Sorry... Wrong answer :("
