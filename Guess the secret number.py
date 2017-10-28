ddee3d013151ee3f6a4ae730837a0bda3dcb35cf
mariankata <32468957+mariankata@users.noreply.github.com>
2017-10-29 01:24:58 +0200

    Update Guess the secret number.py

--- Guess the secret number.py
+++ Guess the secret number.py
@@ -1,9 +1,11 @@
 import random
 secret = random.randint(1,10)
 #print secret
-guess = int(raw_input("Guess the secret number (1-10): "))
-
-if secret == guess:
-    print "Congratulations!!! You guessed the secret number :)"
-else :
-    print "Sorry... Wrong answer :("
+print "\nHi, let\'s play a game :)\n"
+while True:
+    guess = int(raw_input("Guess the secret number (1-10): "))
+    if secret == guess:
+        print "\nCongratulations!!! You guessed the secret number :)\n"
+        break
+    else :
+        print "\nSorry... Wrong answer :( \nTry again\n"
